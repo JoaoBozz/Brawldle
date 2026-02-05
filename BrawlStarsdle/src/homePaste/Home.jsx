4e8f47eb33062cfb3d82c345a852f388f0b4ce5d
JoaoBozz <163414633+JoaoBozz@users.noreply.github.com>
2026-02-05 15:55:09 -0300

    Terminei Header

--- BrawlStarsdle/src/homePaste/Home.jsx
+++ BrawlStarsdle/src/homePaste/Home.jsx
@@ -4,7 +4,9 @@ function Home() {
     return (
         <div className={style.home}>
             <header>
-
+                <span class="material-icons">settings</span>
+                <img src="/src/assets/logoBrawldle.png" alt="Logo Brawldle"/>
+                <span class="material-icons">language</span>
             </header>
             <div className={style.container}>
 
